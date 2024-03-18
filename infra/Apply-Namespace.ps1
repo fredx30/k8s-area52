77cc8c7f4cb585110f8de616e672d65c4973008e
Fredrik Dyrvold <fredrik@dyrvold.dev>
2024-03-18 18:13:08 +0100

    Rerun and fixes for 2024-03-18.

--- infra/Apply-Namespace.ps1
+++ infra/Apply-Namespace.ps1
@@ -1,21 +1,21 @@
-
-param()
-
-
-k apply -f ingress-lb.yaml
-
 helm repo add jetstack https://charts.jetstack.io
 helm repo update
+helm install `
+  cert-manager jetstack/cert-manager `
+  --namespace cert-manager `
+  --create-namespace `
+  --version v1.14.4 `
+  --set installCRDs=true `
+  --set ingressShim.defaultIssuerName=dyrvold-dev-zerossl `
+  --set ingressShim.defaultIssuerKind=ClusterIssuer
 
-helm install cert-manager jetstack/cert-manager --namespace cert-manager --create-namespace `
-    --version v1.9.1 --set installCRDs=true
 
 Write-host "Create credentials from zerossl."
 $KID = Read-host "EAB_KID: "
-$HMAC = Read-host "EAB_HMAC_Key: " -AsSecureString | ConvertFrom-SecureString -AsPlainText
+$HMAC = Read-host "EAB_HMAC_Key: "
 kubectl create secret generic zero-ssl-eab-credentials `
   --from-literal "EAB_KID=${KID}" `
   --from-literal "EAB_HMAC_KEY=${HMAC}" `
   -n cert-manager
 
-k apply -f element.ps1
+k apply -f clusterissuer-zerossl.yaml
