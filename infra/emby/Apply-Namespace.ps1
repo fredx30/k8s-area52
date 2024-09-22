d5d39b215a689057b2f9bd09893a0455a094b7ea
Fredrik Dyrvold <fredrik@dyrvold.dev>
2024-09-22 11:16:04 +0200

    Swap helm install for upgrade.

--- infra/emby/Apply-Namespace.ps1
+++ infra/emby/Apply-Namespace.ps1
@@ -1,2 +1,2 @@
 
-helm install emby ../emby-chart --create-namespace --namespace emby --values .\emby-chart-values.yaml
+helm upgrade --install emby ../emby-chart --create-namespace --namespace emby --values .\emby-chart-values.yaml
